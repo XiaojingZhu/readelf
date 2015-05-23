47d6c2e63217e341a4ba552c433c3a3d90a94346
161330114-Wen Zhihao <zh.wen@hotmail.com>
2015-05-23 23:41:22 +0800

    add ELFinfo file  and finish the OS/ABI code

---
+++ elfinfo.h
@@ -0,0 +1,34 @@
+/* Values for e_type, which identifies the object file type.  */
+
+#define ET_NONE		0	/* No file type */
+#define ET_REL		1	/* Relocatable file */
+#define ET_EXEC		2	/* Executable file */
+#define ET_DYN		3	/* Shared object file */
+#define ET_CORE		4	/* Core file */
+#define ET_LOOS		0xFE00	/* Operating system-specific */
+#define ET_HIOS		0xFEFF	/* Operating system-specific */
+#define ET_LOPROC	0xFF00	/* Processor-specific */
+#define ET_HIPROC	0xFFFF	/* Processor-specific */
+
+
+/* EI_OSABI	7	 Operating System/ABI indication */
+#define ELFOSABI_NONE	      0	/* UNIX System V ABI */
+#define ELFOSABI_HPUX	      1	/* HP-UX operating system */
+#define ELFOSABI_NETBSD	      2	/* NetBSD */
+#define ELFOSABI_GNU	      3	/* GNU */
+#define ELFOSABI_LINUX	      3	/* Alias for ELFOSABI_GNU */
+#define ELFOSABI_SOLARIS      6	/* Solaris */
+#define ELFOSABI_AIX	      7	/* AIX */
+#define ELFOSABI_IRIX	      8	/* IRIX */
+#define ELFOSABI_FREEBSD      9	/* FreeBSD */
+#define ELFOSABI_TRU64	     10	/* TRU64 UNIX */
+#define ELFOSABI_MODESTO     11	/* Novell Modesto */
+#define ELFOSABI_OPENBSD     12	/* OpenBSD */
+#define ELFOSABI_OPENVMS     13	/* OpenVMS */
+#define ELFOSABI_NSK	     14	/* Hewlett-Packard Non-Stop Kernel */
+#define ELFOSABI_AROS	     15	/* AROS */
+#define ELFOSABI_FENIXOS     16 /* FenixOS */
+#define ELFOSABI_C6000_ELFABI 64 /* Bare-metal TMS320C6000 */
+#define ELFOSABI_C6000_LINUX 65 /* Linux TMS320C6000 */
+#define ELFOSABI_ARM	     97	/* ARM */
+#define ELFOSABI_STANDALONE 255	/* Standalone (embedded) application */
--- readelf.c
+++ readelf.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"elfinfo.h"  //储存相关信息的表
 #define EI_NIDENT  16
 
 typedef struct{
@@ -35,20 +36,87 @@ void read(char *filename,Elf32_Ehdr *elf_header)
 
 void print(Elf32_Ehdr *elf_header)
 {
+	int i;
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for(int i = 0; i < 16; i++)
+	for( i = 0; i < 16; i++)
 	{
-		printf("%x ",elf_header->ident[i];
+		if(elf_header->e_ident[i] > 0xe)
+			printf("%x ",elf_header->e_ident[i]);
+		else
+			printf("0%x ",elf_header->e_ident[i]);
 	}	
 	printf("\n");
 
 	printf("Class:\t\t\t");
-	for(int i = 0; i< 3; i++)
+	for( i = 0; i< 3; i++)
 	{
-		printf("%c",elf_header->ident[i+1];
+		printf("%c",elf_header->e_ident[i+1]);
 	}
-
+	switch(elf_header->e_ident[4])
+	{
+		case 0: printf("Invalid class\n");
+				break;
+		case 1: printf("32\n");
+				break;
+		case 2: printf("64\n");
+	}
+	
+	printf("Data:\t\t\t");
+	switch(elf_header->e_ident[5])
+	{
+		case 0: printf("Invalid data encoding\n");break;
+		case 1: printf("2's complement, lttle endian(LSB)\n");break;
+		case 2: printf("2's complement, large endian(MSB)\n");break;
+	}
+	printf("Version:\t\t\t");
+	switch(elf_header->e_ident[6])
+	{
+		case 0:printf("%d (invalid)\n",elf_header->e_version);break;
+		case 1:printf("%d (current)\n",elf_header->e_version);break;
+	}
+	
+	printf("OS/ABI:\t\t");
+	switch(elf_header->e_ident[7])
+	{
+	case ELFOSABI_NONE:
+	    printf("UNIX System V\n ");
+	    break;
+	case ELFOSABI_HPUX:
+	    printf("HP-UX operating system\n ");
+	    break;
+	case ELFOSABI_NETBSD:
+	    printf("NetBSD \n");
+	    break;
+	case ELFOSABI_LINUX:
+	    printf("GNU/Alias for ELFOSABI_GNU \n");
+	    break;
+	case ELFOSABI_SOLARIS:
+	    printf("Solaris\n ");
+	    break;
+	case ELFOSABI_AIX:
+	    printf("AIX \n");
+	    break;
+	case ELFOSABI_IRIX:
+	    printf("IRIX\n ");
+	    break;
+	default:
+	    printf("Other\n");
+	    break;
+	    /* remaining */
+	    //#define ELFOSABI_FREEBSD      9	/* FreeBSD */
+	    //#define ELFOSABI_TRU64	     10	/* TRU64 UNIX */
+	    //#define ELFOSABI_MODESTO     11	/* Novell Modesto */
+	    //#define ELFOSABI_OPENBSD     12	/* OpenBSD */
+	    //#define ELFOSABI_OPENVMS     13	/* OpenVMS */
+	    //#define ELFOSABI_NSK	     14	/* Hewlett-Packard Non-Stop Kernel */
+	    //#define ELFOSABI_AROS	     15	/* AROS */
+	    //#define ELFOSABI_FENIXOS     16 /* FenixOS */
+	    //#define ELFOSABI_C6000_ELFABI 64 /* Bare-metal TMS320C6000 */
+	    //#define ELFOSABI_C6000_LINUX 65 /* Linux TMS320C6000 */
+	    //#define ELFOSABI_ARM	     97	/* ARM */
+	    //#define ELFOSABI_STANDALONE 255	/* Standalone (embedded) application *
+	}
 }
 
 int main(int argc, char *argv[])
